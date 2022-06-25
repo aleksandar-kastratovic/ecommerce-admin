24dae871ce20c7aa1b2e7dbf4d514a9c2ceafb20
Predrag S <predrag.stosic@croonus.com>
2022-06-25 11:38:40 +0200

    develop-tests align mock navigate

--- src/pages/B2Bsettings/B2Bsettings.test.jsx
+++ src/pages/B2Bsettings/B2Bsettings.test.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import { shallow } from "enzyme";
 
 import B2Bsettings from "./B2Bsettings";
 
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    useNavigate: jest.fn(),
+  };
+});
+
 describe("<B2Bsettings />", () => {
+  beforeEach(async () => {
+    // gets executed before each 'it' (test)
+    jest.resetAllMocks();
+    useNavigate.mockImplementation(() => ({ navigate: "/B2B-settings" }));
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it("should render successfully", async () => {
     const wrapper = shallow(<B2Bsettings />);
     expect.assertions(1);
--- src/pages/B2Bsettings/DetailsForm/DetailsForm.test.jsx
+++ src/pages/B2Bsettings/DetailsForm/DetailsForm.test.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import { shallow } from "enzyme";
 
 import DetailsForm from "./DetailsForm";
 
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    useNavigate: jest.fn(),
+  };
+});
+
 describe("<DetailsForm />", () => {
+  beforeEach(async () => {
+    // gets executed before each 'it' (test)
+    jest.resetAllMocks();
+    useNavigate.mockImplementation(() => ({ navigate: "/B2B-settings" }));
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it("should render successfully", async () => {
     const wrapper = shallow(<DetailsForm />);
     expect.assertions(1);
