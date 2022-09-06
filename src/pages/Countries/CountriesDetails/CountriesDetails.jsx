e85dca0df0ce5dfe798d9d7c2922269ec5b24f32
marko <praksa@croonus.com>
2022-09-06 16:56:22 +0200

    Countries - api connecting and fields change

--- src/pages/Countries/CountriesDetails/CountriesDetails.jsx
+++ src/pages/Countries/CountriesDetails/CountriesDetails.jsx
@@ -1,116 +1,69 @@
-import { Box } from "@mui/system";
 import { useNavigate, useParams } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
-import CreateForm from "../../../components/shared/Form/CreateForm";
-import Button from "../../../components/shared/Button/Button";
-import PageWrapper from "../../../components/shared/Layout/PageWrapper/PageWrapper";
-import { formatDate } from "../../../helpers/dateFormat";
-
-import fields from "./formField.json";
-import { getCountry, saveCountry } from "../services";
-import AuthContext from "../../../store/auth-contex";
-import requirePropFactory from "@mui/utils/requirePropFactory";
-import { isEmpty } from "lodash";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import FormWrapper from "../../../components/shared/Layout/FormWrapper/FormWrapper";
+import Form from "../../../components/shared/Form/Form";
+import LoadingForm from "../../../components/shared/Loading/LoadingForm";
+import useAPI from "../../../api/api";
 
-const required = [];
-const init = {
-  slug: "",
-  name: "",
-  phone_code: "",
-  source: "",
-  "id_source ": null,
-  id_source: 0,
-};
+import formFields from "./formField.json";
+
 const CountriesDetails = () => {
   const { cid } = useParams();
   const navigate = useNavigate();
-  const handleBack = () => {
-    navigate("/countries");
+  const api = useAPI();
+
+  const init = {
+    id: null,
+    slug: null,
+    name: null,
+    id_country: null,
+    phone_code: null,
+    source: null,
+    id_source: null,
+    status: null,
   };
+
   const [data, setData] = useState(init);
-  const [inputsError, setInputsError] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const { user } = useContext(AuthContext);
 
-  const formItemChangeHandler = ({ target }, type) => {
-    if (type === "date") {
-      setData({ ...data, [target.name]: formatDate(target.value) });
-    } else if (type) {
-      setData({ ...data, [target.name]: target.checked });
-    } else {
-      setData({ ...data, [target.name]: target.value });
-    }
-  };
-
-  const handleData = async () => {
-    try {
-      setIsLoading(true);
-      let response = await getCountry(user.access_token, cid);
-      let { payload } = response.data;
-      setData(payload);
-    } catch (error) {
-      console.warn(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const saveData = async () => {
-    try {
-      let response = await saveCountry(user.access_token, data);
-      handleBack();
-      toast.success("Uspešno uneta forma!");
-    } catch (error) {
-      console.warn(error.response);
-      toast.warning("Greška ");
-    }
-  };
-
-  const onSubmit = () => {
-    const errors = {};
-    Object.keys(data).forEach((prop_name) => {
-      if (isEmpty(data[prop_name])) {
-        if (required.includes(prop_name))
-          errors[prop_name] = {
-            content: "Polje je obavezno, molim Vas unesite vrednost.",
-          };
-      }
-    });
-    isEmpty(errors) ? saveData() : setInputsError(errors);
+  const submitHandler = (data) => {
+    api
+      .post("admin/countries", data)
+      .then((response) => {
+        toast.success("Uspešno");
+        setData(response?.payload);
+      })
+      .catch((error) => {
+        toast.warning("Greška");
+        console.warn(error);
+      });
   };
 
   useEffect(() => {
-    if (cid !== "new") {
-      handleData();
-    }
+    setIsLoading(true);
+
+    api
+      .get(`admin/countries/${cid}`)
+      .then((response) => {
+        setData(response?.payload);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+
+    setIsLoading(false);
   }, []);
 
   return (
-    <PageWrapper title="Detalji države" back={handleBack}>
-      <Box component="form" autoComplete="off">
-        {fields &&
-          fields
-            .filter(({ in_details }) => in_details)
-            .map((item, index) => {
-              return (
-                <CreateForm
-                  data-test-id="admin-form"
-                  onChangeHandler={formItemChangeHandler}
-                  item={item}
-                  key={index}
-                  error={inputsError[item.prop_name]}
-                  value={
-                    Array.isArray(item) && data
-                      ? data[item.prop_name]
-                      : data[item.prop_name]
-                  }
-                />
-              );
-            })}
-        <Button label="Sačuvaj" onClick={onSubmit} />
-      </Box>
-    </PageWrapper>
+    <FormWrapper
+      title={cid === "new" ? "Unos nove države" : data?.name}
+      back={() => {
+        navigate(-1);
+      }}
+    >
+      {!isLoading ? <Form formFields={formFields} initialData={data} onSubmit={submitHandler} /> : <LoadingForm fields={formFields.length} />}
+    </FormWrapper>
   );
 };
 
