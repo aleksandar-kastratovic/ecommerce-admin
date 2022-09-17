4ba2a443a22279c99a4ac0b7139430f90929d2d5
marko <praksa@croonus.com>
2022-09-17 15:15:41 +0200

    index js changed api path from local to staging - only for development purpose

--- src/index.js
+++ src/index.js
@@ -9,19 +9,16 @@ const staging = "https://api.staging.croonus.com/api/v1/";
 
 // Assert sure base URL for the API set
 if (process.env.REACT_APP_URL || local) {
-  localStorage.setItem("api", process.env.REACT_APP_URL || local);
+    localStorage.setItem("api", process.env.REACT_APP_URL || staging);
 
-  ReactDOM.render(
-    <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthContextProvider>,
-    document.getElementById("root")
-  );
+    ReactDOM.render(
+        <AuthContextProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthContextProvider>,
+        document.getElementById("root")
+    );
 } else {
-  ReactDOM.render(
-    <h1>Required env variable is not set: REACT_APP_URL</h1>,
-    document.getElementById("root")
-  );
+    ReactDOM.render(<h1>Required env variable is not set: REACT_APP_URL</h1>, document.getElementById("root"));
 }
