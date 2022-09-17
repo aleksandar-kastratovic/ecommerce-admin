90b4302f88fcfe95dde120fec6fb2a2b748c3af7
marko <praksa@croonus.com>
2022-09-17 14:04:36 +0200

    Form Wrapper -  added ready prop for loading

--- src/components/shared/Layout/FormWrapper/FormWrapper.jsx
+++ src/components/shared/Layout/FormWrapper/FormWrapper.jsx
@@ -2,14 +2,14 @@ import { Box } from "@mui/material";
 import PageWrapper from "../PageWrapper/PageWrapper";
 import styles from "./FormWrapper.module.scss";
 
-const FormWrapper = ({ title, back, children, actions }) => {
-  return (
-    <PageWrapper title={title} back={back} actions={actions}>
-      <Box className={styles.formWrapper}>
-        <Box className={styles.formContainer}>{children}</Box>
-      </Box>
-    </PageWrapper>
-  );
+const FormWrapper = ({ title, back, children, actions, ready }) => {
+    return (
+        <PageWrapper title={title} back={back} actions={actions} ready={ready}>
+            <Box className={styles.formWrapper}>
+                <Box className={styles.formContainer}>{children}</Box>
+            </Box>
+        </PageWrapper>
+    );
 };
 
 export default FormWrapper;
