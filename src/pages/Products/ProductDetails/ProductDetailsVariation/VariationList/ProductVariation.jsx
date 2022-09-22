93a1e60ef4d581a8fcd00c543d10945c581af8eb
marko <praksa@croonus.com>
2022-09-22 12:20:22 +0200

    id_location -> id_store

--- src/pages/Products/ProductDetails/ProductDetailsVariation/VariationList/ProductVariation.jsx
+++ src/pages/Products/ProductDetails/ProductDetailsVariation/VariationList/ProductVariation.jsx
@@ -11,114 +11,105 @@ import basicData from "../../forms/product_variant_basic.json";
 import styles from "./VariationList.module.scss";
 
 const ProductVariation = ({ title = "", idProduct, idProductVariant }) => {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const pricesInit = {
-    id: null,
-    id_product: idProduct,
-    id_product_variant: idProductVariant,
-    system: "",
-    country: 1,
-    currency: "",
-    type: "",
-    group: "",
-    price_single_with_out_vat: 0,
-    price_single_with_vat: 0,
-    price_vat_procent: 0,
-    price_quantity: null,
-    price_unit: "",
-    active_to: "",
-    price_with_out_vat: 0,
-    price_with_vat: 0,
-  };
+    const pricesInit = {
+        id: null,
+        id_product: idProduct,
+        id_product_variant: idProductVariant,
+        system: "",
+        country: 1,
+        currency: "",
+        type: "",
+        group: "",
+        price_single_with_out_vat: 0,
+        price_single_with_vat: 0,
+        price_vat_procent: 0,
+        price_quantity: null,
+        price_unit: "",
+        active_to: "",
+        price_with_out_vat: 0,
+        price_with_vat: 0,
+    };
 
-  const seoInit = {
-    id: null,
-    id_product: idProduct,
-    id_product_variant: idProductVariant,
-    id_country: 0,
-    lang: "",
-    slug: "",
-    meta_title: "",
-    meta_keywords: "",
-    meta_description: "",
-    meta_url: "",
-    active: 1,
-    order: 1,
-  };
+    const seoInit = {
+        id: null,
+        id_product: idProduct,
+        id_product_variant: idProductVariant,
+        id_country: 0,
+        lang: "",
+        slug: "",
+        meta_title: "",
+        meta_keywords: "",
+        meta_description: "",
+        meta_url: "",
+        active: 1,
+        order: 1,
+    };
 
-  const lagerInit = {
-    id: null,
-    id_product: idProduct,
-    id_product_variant: idProductVariant,
-    id_location: null,
-    quantity: 0,
-    unit: "",
-  };
+    const lagerInit = {
+        id: null,
+        id_product: idProduct,
+        id_product_variant: idProductVariant,
+        id_store: null,
+        quantity: 0,
+        unit: "",
+    };
 
-  return (
-    <Box className={styles.productVariation}>
-      <Box
-        className={styles.productVariationTitle}
-        onClick={() => setOpen(!open)}
-      >
-        {title} {open ? <Icon>expand_less</Icon> : <Icon>expand_more</Icon>}
-      </Box>
-      {open && (
-        <Box className={styles.productVariationBody}>
-          <VariationSection
-            title="Osnovni podaci"
-            formFields={basicData}
-            type="form"
-            getUrl={`admin/productitems/variants/basic_data/${idProduct}/${idProductVariant}`}
-            postUrl="admin/productitems/variants/basic_data/"
-            idProduct={idProduct}
-            idProductVariant={idProductVariant}
-          />
-          <VariationSection
-            title="Cena"
-            formFields={prices}
-            type="list"
-            listUrl={`admin/productitems/variants/prices/${idProduct}/${idProductVariant}`}
-            postUrl={"admin/productitems/variants/prices/"}
-            deleteUrl={"admin/productitems/variants/prices"}
-            init={pricesInit}
-            idProduct={idProduct}
-            idProductVariant={idProductVariant}
-          />
-          <VariationSection
-            title="Lager"
-            formFields={lager}
-            type="list"
-            listUrl={`admin/productitems/variants/inventory/${idProduct}/${idProductVariant}`}
-            postUrl={"admin/productitems/variants/inventory/"}
-            deleteUrl={"admin/productitems/variants/inventory"}
-            init={lagerInit}
-            idProduct={idProduct}
-            idProductVariant={idProductVariant}
-          />
-          <VariationSection
-            title="Seo"
-            formFields={seo}
-            type="list"
-            listUrl={`admin/productitems/variants/seo/${idProduct}/${idProductVariant}`}
-            postUrl={"admin/productitems/variants/seo/"}
-            deleteUrl={"admin/productitems/variants/seo"}
-            init={seoInit}
-            idProduct={idProduct}
-            idProductVariant={idProductVariant}
-          />
-          <VariationSection
-            title="Galerija"
-            formFields={gallery}
-            type="form"
-            idProduct={idProduct}
-            idProductVariant={idProductVariant}
-          />
+    return (
+        <Box className={styles.productVariation}>
+            <Box className={styles.productVariationTitle} onClick={() => setOpen(!open)}>
+                {title} {open ? <Icon>expand_less</Icon> : <Icon>expand_more</Icon>}
+            </Box>
+            {open && (
+                <Box className={styles.productVariationBody}>
+                    <VariationSection
+                        title="Osnovni podaci"
+                        formFields={basicData}
+                        type="form"
+                        getUrl={`admin/productitems/variants/basic_data/${idProduct}/${idProductVariant}`}
+                        postUrl="admin/productitems/variants/basic_data/"
+                        idProduct={idProduct}
+                        idProductVariant={idProductVariant}
+                    />
+                    <VariationSection
+                        title="Cena"
+                        formFields={prices}
+                        type="list"
+                        listUrl={`admin/productitems/variants/prices/${idProduct}/${idProductVariant}`}
+                        postUrl={"admin/productitems/variants/prices/"}
+                        deleteUrl={"admin/productitems/variants/prices"}
+                        init={pricesInit}
+                        idProduct={idProduct}
+                        idProductVariant={idProductVariant}
+                    />
+                    <VariationSection
+                        title="Lager"
+                        formFields={lager}
+                        type="list"
+                        listUrl={`admin/productitems/variants/inventory/${idProduct}/${idProductVariant}`}
+                        postUrl={"admin/productitems/variants/inventory/"}
+                        deleteUrl={"admin/productitems/variants/inventory"}
+                        init={lagerInit}
+                        idProduct={idProduct}
+                        idProductVariant={idProductVariant}
+                    />
+                    <VariationSection
+                        title="Seo"
+                        formFields={seo}
+                        type="list"
+                        listUrl={`admin/productitems/variants/seo/${idProduct}/${idProductVariant}`}
+                        postUrl={"admin/productitems/variants/seo/"}
+                        deleteUrl={"admin/productitems/variants/seo"}
+                        init={seoInit}
+                        idProduct={idProduct}
+                        idProductVariant={idProductVariant}
+                    />
+                    <VariationSection title="Galerija" formFields={gallery} type="form" idProduct={idProduct} idProductVariant={idProductVariant} />
+                </Box>
+            )}
         </Box>
-      )}
-    </Box>
-  );
+    );
 };
 
 export default ProductVariation;
