6b53977ab8dcca75d0e4e3755812fe2bb4036c8c
marko <praksa@croonus.com>
2022-09-23 09:20:20 +0200

    Pages fixes

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,20 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import ListPage from "../../components/shared/ListPage/ListPage";
-import tblFields from './tblFields.json'; 
-
-
+import tblFields from "./tblFields.json";
 
 const News = () => {
     const navigate = useNavigate();
     const categoryPage = () => {
         navigate("/news/category");
-      };
+    };
 
     const categoryButtons = [{ id: 1, label: "Kategorije", action: categoryPage }];
 
-    return(
-        <ListPage apiUrl="admin/news-b2c/news" title="Lista vesti" columnFields={tblFields}  additionalButtons={categoryButtons}/>
-    )
+    return <ListPage apiUrl="admin/news-b2c/news" title="Lista vesti" columnFields={tblFields} additionalButtons={categoryButtons} />;
 };
 
 export default News;
