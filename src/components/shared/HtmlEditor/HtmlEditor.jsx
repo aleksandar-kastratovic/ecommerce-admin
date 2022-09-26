b80359dc04e113f58c6678cffd81cd0dd2b563dd
marko <praksa@croonus.com>
2022-09-26 11:06:03 +0200

    Html editor fix

--- src/components/shared/HtmlEditor/HtmlEditor.jsx
+++ src/components/shared/HtmlEditor/HtmlEditor.jsx
@@ -16,8 +16,9 @@ const HtmlEditor = ({ name, value, onChange, disabled }) => {
                 init={{
                     height: 500,
                     menubar: false,
-                    plugins: "advlist autolink lists link image charmap print preview anchor | searchreplace visualblocks code fullscreen | insertdatetime media table paste code help wordcount",
-                    toolbar: "undo redo | formatselect | " + "bold italic backcolor | alignleft aligncenter " + "alignright alignjustify | bullist numlist outdent indent | " + "removeformat | help",
+                    plugins: "advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount",
+                    toolbar:
+                        "undo redo | code fullscreen | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
                     content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
                 }}
             />
