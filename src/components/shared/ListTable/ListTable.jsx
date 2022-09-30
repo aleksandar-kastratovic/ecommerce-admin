535791fccf488592e54e63e7240d6baee55d13b3
marko <praksa@croonus.com>
2022-09-30 16:17:23 +0200

    B2B settings  - fixes and api connecting

--- src/components/shared/ListTable/ListTable.jsx
+++ src/components/shared/ListTable/ListTable.jsx
@@ -1,48 +1,36 @@
-import React, { useState } from "react"
-import Table from "@mui/material/Table"
-import TableContainer from "@mui/material/TableContainer"
-import ListPagination from "./ListPagination/ListPagination"
-import ListTableBody from "./ListTableBody"
-import ListTableHead from "./ListTableHead"
-import styles from "./ListTable.module.scss"
+import React, { useState } from "react";
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+import ListPagination from "./ListPagination/ListPagination";
+import ListTableBody from "./ListTableBody";
+import ListTableHead from "./ListTableHead";
+import styles from "./ListTable.module.scss";
 
-const ListTable = ({ fields = [], listData = [], isLoading = false, onPageChange, handleActions }) => {
-    const { items, pagination } = listData
-    const [ order, setOrder ] = useState("asc")
-    const [ orderBy, setOrderBy ] = useState("name")
+const ListTable = ({ fields = [], listData = [], isLoading = false, onPageChange, handleActions, previewColumn }) => {
+    const { items, pagination } = listData;
+    const [order, setOrder] = useState("asc");
+    const [orderBy, setOrderBy] = useState("name");
 
     const handleSort = (event, property) => {
-        const isAsc = orderBy === property && order === "asc"
-        setOrder(isAsc ? "desc" : "asc")
-        setOrderBy(property)
-    }
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    };
 
     // Show the table
     return (
         <>
             <TableContainer className={styles.wrapper}>
                 <Table>
+                    <ListTableHead fields={fields} order={order} orderBy={orderBy} onRequestSort={handleSort} rowCount={fields.length} />
 
-                    <ListTableHead
-                        fields={fields}
-                        order={order}
-                        orderBy={orderBy}
-                        onRequestSort={handleSort}
-                        rowCount={fields.length} />
-
-                    <ListTableBody
-                        items={items ?? []}
-                        fields={fields}
-                        isLoading={isLoading}
-                        handleActions={handleActions}
-                        error={null} />
-
+                    <ListTableBody items={items ?? []} fields={fields} isLoading={isLoading} handleActions={handleActions} error={null} previewColumn={previewColumn} />
                 </Table>
             </TableContainer>
 
             <ListPagination pagination={pagination} onPageChange={onPageChange} />
         </>
-    )
-}
+    );
+};
 
-export default ListTable
+export default ListTable;
