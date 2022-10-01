edf07d2e675b4eb1ff64f230d4c59907163b4c17
marko <praksa@croonus.com>
2022-10-01 18:44:08 +0200

    Temprarly hidden New button on B2B and B2C cfg

--- src/pages/B2Bsettings/B2Bsettings.jsx
+++ src/pages/B2Bsettings/B2Bsettings.jsx
@@ -2,7 +2,7 @@ import ListPage from "../../components/shared/ListPage/ListPage";
 import fields from "./mainListFields.json";
 
 const B2Bsettings = () => {
-    return <ListPage apiUrl="admin/configuration-b2b/main" title="B2B podešavanja" columnFields={fields} previewColumn="module" />;
+    return <ListPage apiUrl="admin/configuration-b2b/main" title="B2B podešavanja" columnFields={fields} previewColumn="module" showNewButton={false} />;
 };
 
 export default B2Bsettings;
--- src/pages/B2CSettings/B2CSettings.jsx
+++ src/pages/B2CSettings/B2CSettings.jsx
@@ -2,7 +2,7 @@ import ListPage from "../../components/shared/ListPage/ListPage";
 import fields from "./mainListFields.json";
 
 const B2Csettings = () => {
-    return <ListPage apiUrl="admin/configuration-b2c/main" title="B2C podešavanja" columnFields={fields} previewColumn="module" />;
+    return <ListPage apiUrl="admin/configuration-b2c/main" title="B2C podešavanja" columnFields={fields} previewColumn="module" showNewButton={false} />;
 };
 
 export default B2Csettings;
