53e435b33130205dfda79415c725c0e4bb786de8
marko <praksa@croonus.com>
2022-10-02 11:23:20 +0200

    Prices round to 2 decimals

--- src/pages/Products/ProductDetails/panels/Prices.jsx
+++ src/pages/Products/ProductDetails/panels/Prices.jsx
@@ -80,22 +80,20 @@ const Prices = ({ productId }) => {
             case "price_single_with_out_vat":
             case "price_vat_procent":
             case "price_quantity":
-                ret.price_with_out_vat = ret.price_quantity * ret.price_single_with_out_vat;
-                ret.price_with_vat = (ret.price_vat_procent / 100 + 1) * ret.price_with_out_vat;
+                ret.price_with_out_vat = Math.round(ret.price_quantity * ret.price_single_with_out_vat * 100) / 100;
+                ret.price_with_vat = Math.round((ret.price_vat_procent / 100 + 1) * ret.price_with_out_vat * 100) / 100;
                 return ret;
             case "price_with_out_vat":
-                ret.price_with_vat = (ret.price_vat_procent / 100 + 1) * ret.price_with_out_vat;
-                ret.price_single_with_out_vat = ret.price_with_out_vat / ret.price_quantity;
+                ret.price_with_vat = Math.round((ret.price_vat_procent / 100 + 1) * ret.price_with_out_vat * 100) / 100;
+                ret.price_single_with_out_vat = Math.round((ret.price_with_out_vat / ret.price_quantity) * 100) / 100;
                 return ret;
             case "price_with_vat":
-                ret.price_with_out_vat = ret.price_with_vat / (ret.price_vat_procent / 100 + 1);
-                ret.price_single_with_out_vat = ret.price_with_out_vat / ret.price_quantity;
+                ret.price_with_out_vat = Math.round((ret.price_with_vat / (ret.price_vat_procent / 100 + 1)) * 100) / 100;
+                ret.price_single_with_out_vat = Math.round((ret.price_with_out_vat / ret.price_quantity) * 100) / 100;
                 return ret;
             default:
                 return ret;
         }
-
-        return { ...data, price_with_vat: 170 };
     };
 
     return <List formFields={formFields} init={init} listFields={listData} onSave={handleSubmit} onDelete={handleDelete} additionalButtons={additionalButtons} validateData={validateData} />;
