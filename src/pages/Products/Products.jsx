3ba98a893d0f98f76628fd351121013f90a13031
marko <praksa@croonus.com>
2022-10-07 14:28:02 +0200

    Bug fixes rebate tiers, product list

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,7 +3,7 @@ import ListPage from "../../components/shared/ListPage/ListPage";
 import tblFields from "./ProductColumnFields.json";
 
 const Products = () => {
-    return <ListPage apiUrl="admin/product-items/list" title="Proizvodi" columnFields={tblFields} />;
+    return <ListPage apiUrl="admin/product-items/list" deleteUrl="admin/product-items/basic-data" title="Proizvodi" columnFields={tblFields} />;
 };
 
 export default Products;
