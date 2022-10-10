bf097246c995d9a7702473534f5e3686ab321702
marko <praksa@croonus.com>
2022-10-10 10:15:43 +0200

    Added select all option

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { removeAtIndex } from "../helpers/data"
+import { useState } from "react";
+import { removeAtIndex } from "../helpers/data";
 
 /**
  * Used to store the list of items with useful methods.
@@ -9,34 +9,34 @@ import { removeAtIndex } from "../helpers/data"
  * @return {[list: [], set: function(id), unset: function(id), has: function(id)]} The list of controls for the list.
  */
 const useFileInput = (initialList: [] = []) => {
-    const [ state, setState ] = useState(initialList)
+    const [state, setState] = useState(initialList);
 
     // Set a value
-    const set = id => {
+    const set = (id) => {
         if (!state.includes(id)) {
-            setState(state => [ ...state, id ])
+            setState((state) => [...state, id]);
         }
-    }
+    };
 
     // Unset a value
-    const unset = id => {
-        const index = state.findIndex(item => item === id)
+    const unset = (id) => {
+        const index = state.findIndex((item) => item === id);
         if (index > -1) {
-            setState(state => removeAtIndex(state, index))
+            setState((state) => removeAtIndex(state, index));
         }
-    }
+    };
 
     // Toggle the urrent value
-    const toggle = id => {
-        state.includes(id)
-            ? unset(id)
-            : set(id)
-    }
+    const toggle = (id) => {
+        state.includes(id) ? unset(id) : set(id);
+    };
 
     // Check if list has an item
-    const has = id => state.includes(id)
+    const has = (id) => state.includes(id);
 
-    return { list: state, set, unset, toggle, has }
-}
+    const clear = () => setState([]);
 
-export default useFileInput
+    return { list: state, set, unset, toggle, has, clear };
+};
+
+export default useFileInput;
