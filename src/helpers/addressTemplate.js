a08d59a68f048f6c74a4622e460fbf09860bd8aa
Marko Lazarević <marko.lazarevic@croonus.com>
2022-11-15 11:12:00 +0100

    B2BOrders Details - changed fields

--- src/helpers/addressTemplate.js
+++ src/helpers/addressTemplate.js
@@ -1,5 +1,5 @@
 const addressTemplate = (address, object_number, floor, apartmentNumber) => {
-    return `${address} ${object_number} ${floor} ${apartmentNumber}`;
+    return `${address ?? ""} ${object_number ?? ""} ${floor ?? ""} ${apartmentNumber ?? ""}`;
 };
 
 export default addressTemplate;
