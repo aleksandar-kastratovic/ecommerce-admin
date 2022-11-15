1d1ac99c2ca72801cce7b2c3b5cfd09b0ebf6600
Ruza <vasilijevic.ruza.bps.com>
2022-11-15 08:18:01 +0100

    changed index page, put staging instead of local

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const local = "http://25.30.108.95:4400/api/v1/";
 const staging = "https://api.staging.croonus.com/api/v1/";
 
 // Assert sure base URL for the API set
-if (process.env.REACT_APP_URL || local) {
-    localStorage.setItem("api", process.env.REACT_APP_URL || local);
+if (process.env.REACT_APP_URL || staging) {
+    localStorage.setItem("api", process.env.REACT_APP_URL || staging);
 
     ReactDOM.render(
         <AuthContextProvider>
