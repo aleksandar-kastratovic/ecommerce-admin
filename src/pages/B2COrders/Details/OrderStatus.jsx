f7dac8e00e361f4183275c346de110eb0b102ad8
Marko Lazarević <marko.lazarevic@croonus.com>
2022-12-08 10:04:02 +0100

    Order details fixed statuses and messages

--- src/pages/B2COrders/Details/OrderStatus.jsx
+++ src/pages/B2COrders/Details/OrderStatus.jsx
@@ -5,26 +5,28 @@ import { toast } from "react-toastify";
 import useAPI from "../../../api/api";
 import Button from "../../../components/shared/Button/Button";
 import Buttons from "../../../components/shared/Form/Buttons/Buttons";
-import { InputCheckbox, InputSelect, InputText } from "../../../components/shared/Form/FormInputs/FormInputs";
+import { InputCheckbox, InputHtml, InputSelect } from "../../../components/shared/Form/FormInputs/FormInputs";
 import HistoryModal from "./HistoryModal";
 
 const OrderStatus = ({ orderId, status }) => {
     const api = useAPI();
     const apiPath = "admin/orders-b2c/status";
 
-    const init = { id_order: orderId, status, send_to_customer: null, send_default_message: null, title: null, description: "", mail_to_customer: null, mail_to_admin: null };
+    const init = { id_order: orderId, status, send_mail: null, mail_to_customer: null, mail_to_admin: null, subject: null, content: null };
 
     const [data, setData] = useState(init);
-    const [originalMessage, setOriginalMessage] = useState(data.message);
+    const [originalMessage, setOriginalMessage] = useState(data.content);
     const [openDialog, setOpenDialog] = useState({ show: false });
     const [sendMail, setSendMail] = useState("0");
+
+    const [loaded, setLoaded] = useState(false);
 
     const getMessage = (statusCode) => {
         api.get(`${apiPath}/message/${orderId}/${statusCode}`)
             .then((response) => {
-                setOriginalMessage(response?.payload.message);
+                setOriginalMessage(response?.payload.content);
                 setSendMail(response?.payload.send_mail);
-                setData({ ...data, description: response?.payload.message });
+                setData({ ...data, ...response?.payload });
             })
             .catch((error) => console.warn(error));
     };
@@ -34,9 +36,15 @@ const OrderStatus = ({ orderId, status }) => {
     }, [data.status]);
 
     const formSubmitHandler = () => {
-        api.post(apiPath, { ...data, send_default_message: data.description === originalMessage })
+        const ret = {};
+        if (data.send_to_customer) {
+            ret = { ...data, send_default_message: data.content === originalMessage };
+        } else {
+            ret = { ...data, send_default_message: null, mail_to_customer: null, mail_to_customers: null, content: null, subject: null };
+        }
+        api.post(apiPath, ret)
             .then((response) => {
-                setData({ ...data, description: originalMessage, send_to_customer: false });
+                setData({ ...data, content: originalMessage, send_to_customer: false });
                 toast.success("Uspešno!");
             })
             .catch((error) => {
@@ -70,11 +78,15 @@ const OrderStatus = ({ orderId, status }) => {
                         }}
                     />
                     {data.send_to_customer && (
-                        <InputText
+                        <InputHtml
                             label="Poruka"
-                            name="description"
-                            value={data.description ?? ""}
+                            name="content"
+                            value={data.content ?? ""}
                             onChange={({ target }) => {
+                                if (!loaded) {
+                                    setOriginalMessage(target.value);
+                                    setLoaded(true);
+                                }
                                 setData({ ...data, [target.name]: target.value });
                             }}
                         />
