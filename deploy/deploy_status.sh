18cf3dee51bc39b4fc262c08304e8a65f568555d
Željko Glišov <zeljko.glisov@croonus.com>
2023-02-11 13:19:20 +0100

    Pipeline test

--- deploy/deploy_status.sh
+++ deploy/deploy_status.sh
@@ -17,7 +17,4 @@
 echo "First arg: $1"
 echo "Second arg: $2"
 
-
 curl -X POST "https://roverkonfigurator.croonus.com/backend/save.php?ENVIRONMENT=$1&EXIT_CODE=$2"
-   -H "Content-Type: application/json"
-   -d "{\"BITBUCKET_EXIT_CODE\":\"$1\",\"BITBUCKET_EXIT_CODE\":\"$2\"}"
