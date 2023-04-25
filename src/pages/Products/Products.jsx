edaeda7a8f94a184a4f471f3acb68921a9090f1c
Ruza Vasilijevic <vasilijevic.ruza.bps@gmail.com>
2023-04-25 10:07:41 +0200

    Add duplicate icon to product list.

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import ListPage from "../../components/shared/ListPage/ListPage";
 
 import tblFields from "./ProductColumnFields.json";
 
 const Products = () => {
-    return <ListPage apiUrl="admin/product-items/list" deleteUrl="admin/product-items/basic-data" title="Proizvodi" columnFields={tblFields} />;
+  const customActions = {
+    type1: { handler: () => { }, icon: "content_copy" }
+  };
+  return (
+    <ListPage
+      apiUrl="admin/product-items/list"
+      deleteUrl="admin/product-items/basic-data"
+      title="Proizvodi"
+      columnFields={tblFields}
+      customActions={customActions}
+    />
+  );
 };
 
 export default Products;
