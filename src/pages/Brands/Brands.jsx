adb614bceaca4fa6c3437070d6200f5c659ae4cb
Željko Glišov <zeljko.glisov@croonus.com>
2024-01-16 08:14:39 +0100

    Brands / Delete modal

--- src/pages/Brands/Brands.jsx
+++ src/pages/Brands/Brands.jsx
@@ -1,8 +1,47 @@
+import { useContext, useState } from "react";
 import ListPage from "../../components/shared/ListPage/ListPage";
 
 import tblFields from "./tblFields.json";
+import AuthContext from "../../store/auth-contex";
+import ModalContent from "./ModalContent";
+import { toast } from "react-toastify";
 
 const Brands = () => {
+  const authCtx = useContext(AuthContext);
+  const { api } = authCtx;
+
+  const customActions = {
+    delete: {
+      clickHandler: {
+        type: 'dialog_delete',
+        fnc: (rowData, handleDeleteModalData) => {
+          return {
+            show: true,
+            id: rowData.id,
+            mutate: null,
+            children: (
+              <ModalContent apiPath={`admin/brands/message/${rowData.id}`} rowData={rowData} handleDeleteModalData={handleDeleteModalData} />
+            )
+          };
+        },
+      },
+      deleteClickHandler: {
+        type: 'dialog_delete',
+        fnc: (rowData, deleteModalData) => {
+          api.delete(`admin/brands/confirm/${rowData.id}`)
+            .then(() => toast.success("Zapis je uspešno obrisan"))
+            .catch(() => toast.warning("Došlo je do greške prilikom brisanja"));
+
+          return {
+            show: false,
+            id: rowData.id,
+            mutate: 1,
+          };
+        }
+      },
+    },
+  }
+
   return (
     <ListPage
       listPageId="Brands"
@@ -10,6 +49,7 @@ const Brands = () => {
       actionNewButton="modal"
       title="Brendovi"
       columnFields={tblFields}
+      customActions={customActions}
     />
   );
 };
