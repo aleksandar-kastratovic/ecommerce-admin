b5d2a99c1c10c55e9a071ac32ef175797d3009e2
Željko Glišov <zeljko.glisov@croonus.com>
2024-01-18 11:59:20 +0100

    Admin / Category group - Delete modal - nije kompletirano

--- src/pages/Categories/CategoriesGroupsListPage.jsx
+++ src/pages/Categories/CategoriesGroupsListPage.jsx
@@ -1,10 +1,17 @@
-import { useLocation, useNavigate } from "react-router";
+import { useContext } from "react";
+import { useLocation } from "react-router";
 import ListPage from "../../components/shared/ListPage/ListPage";
+
 import tblFields from "./tblFields.json";
+import AuthContext from "../../store/auth-contex";
+import ModalContent from "./ModalContent";
+import { toast } from "react-toastify";
 
 const CategoriesGroupsListPage = () => {
 
   const { pathname } = useLocation();
+  const authCtx = useContext(AuthContext);
+  const { api } = authCtx;
 
   const customActions = {
     list: {
@@ -32,10 +39,44 @@ const CategoriesGroupsListPage = () => {
       },
       icon: "account_tree",
       title: "Drvo kategorija",
+    },
+    delete: {
+      clickHandler: {
+        type: 'dialog_delete',
+        fnc: (rowData, handleDeleteModalData) => {
+          return {
+            show: true,
+            id: rowData.id,
+            mutate: null,
+            children: (
+              <ModalContent apiPath={`admin/category-product/groups/message/${rowData.id}`} rowData={rowData} handleDeleteModalData={handleDeleteModalData} />
+            )
+          };
+        },
+      },
+      deleteClickHandler: {
+        type: 'dialog_delete',
+        fnc: (rowData, deleteModalData) => {
+
+          if(deleteModalData.all_fill) {
+            api.delete(`admin/category-product/groups/confirm/${rowData.id}`, deleteModalData)
+              .then(() => toast.success("Zapis je uspešno obrisan"))
+              .catch(() => toast.warning("Došlo je do greške prilikom brisanja"));
+
+            return {
+              show: false,
+              id: rowData.id,
+              mutate: 1,
+            };
+          } else {
+            toast.warning("Potrebno je da povežete sve opcije koje se brišu");
+            return false;
+          }
+          
+        }
+      },
     }
   };
-
-
 
   return (
     <ListPage
