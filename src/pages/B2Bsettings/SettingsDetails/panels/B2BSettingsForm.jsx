a9569af6cc7cb4715fea3422a6958e52018d9014
Stefan Milijanovic <stefan.milijanovic@croonus.com>
2024-01-29 16:52:02 +0100

    B2B podesavanja - inicijalno otvaranje taba sa leve strane

--- src/pages/B2Bsettings/SettingsDetails/panels/B2BSettingsForm.jsx
+++ src/pages/B2Bsettings/SettingsDetails/panels/B2BSettingsForm.jsx
@@ -1,50 +1,50 @@
 import { useContext, useEffect, useState } from "react";
-import { useQuery } from "react-query";
-import { toast } from "react-toastify";
 import Form from "../../../../components/shared/Form/Form";
 import Loading from "../../../../components/shared/Loading/Loading";
+import { toast } from "react-toastify";
 import AuthContext from "../../../../store/auth-contex";
 
-const B2CSettingsForm = ({ form_slug, module, submodule }) => {
-  const authCtx = useContext(AuthContext);
-  const { api } = authCtx;
+const B2BSettingsForm = ({ form_slug, config_module_id, module, submodule }) => {
+    const authCtx = useContext(AuthContext);
+    const { api } = authCtx;
 
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const [formFields, setFormFields] = useState([]);
-  const [formData, setFormData] = useState([]);
+    const [formFields, setFormFields] = useState([]);
+    const [formData, setFormData] = useState([]);
 
-  const dataHandler = () => {
-    setIsLoading(true);
-    api.get(`admin/form/data/${form_slug}`)
-      .then((response) => {
-        setFormFields(response?.payload);
-        setIsLoading(false);
-      })
-      .catch((error) => console.warn(error));
-    api.get(`admin/configuration-b2b/main/${module}/${submodule}`)
-      .then((response) => setFormData(response?.payload))
-      .catch((error) => console.warn(error));
-  };
+    const dataHandler = () => {
+        setIsLoading(true);
+        api.get(`admin/form/data/${form_slug}`)
+            .then((response) => {
+                setFormFields(response?.payload);
 
-  useEffect(() => {
-    dataHandler();
-  }, []);
+                setIsLoading(false);
+            })
+            .catch((error) => console.warn(error));
+        api.get(`admin/configuration-b2b/main/${module}/${submodule}`)
+            .then((response) => setFormData(response?.payload))
+            .catch((error) => console.warn(error));
+    };
 
-  const initialData = {};
-  for (const item of formData?.items ?? []) {
-    initialData[item.slug] = item.type === "image" ? item?.base64 : item?.value;
-  }
+    useEffect(() => {
+        dataHandler();
+    }, []);
 
-  const submitHandler = (data) => {
-    api.post(`admin/configuration-b2b/main/${module}/${submodule}`, data)
-      .then((response) => {
-        setFormData(response?.payload)
-        toast.success("Uspešno");
-      })
-      .catch((error) => console.warn(error));
-  };
-  return !isLoading ? <Form formFields={formFields} onSubmit={submitHandler} initialData={initialData} /> : <Loading />;
+    const initialData = {};
+    for (const item of formData?.items ?? []) {
+        initialData[item.slug] = item.type === "image" ? item?.base64 : item?.value;
+    }
+
+    const submitHandler = (data) => {
+        api.post(`admin/configuration-b2b/main/${module}/${submodule}`, data)
+            .then((response) => {
+                setFormData(response?.payload);
+                toast.success("Uspešno");
+            })
+            .catch((error) => console.warn(error));
+    };
+    return !isLoading ? <Form formFields={formFields} onSubmit={submitHandler} initialData={initialData} /> : <Loading />;
 };
 
-export default B2CSettingsForm;
+export default B2BSettingsForm;
