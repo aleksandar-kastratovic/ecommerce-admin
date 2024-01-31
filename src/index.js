47d4efc5e2b420dcb77bce5a1dfdb6a0beb2a8e3
Stefan Milijanovic <stefan.milijanovic@croonus.com>
2024-01-31 09:09:34 +0100

    Global - root render promenjen u skladu sa verzijom react-a

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import App from "./App";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./assets/scss/index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./store/auth-contex";
 
 const local = "http://192.168.1.174:4400/api/v1/";
-const net = "http://25.19.215.162:4400/api/v1/";
-const staging = "https://api.staging.croonus.com/api/v1/";
-const matija = "http://192.168.1.83:4400/api/v1/";
-const dejan = "http://192.168.1.223:4400/api/v1/";
-const bojke = "http://192.168.1.249:4400/api/v1/";
-const hamachi_zeljko = "http://25.59.211.111:4400/api/v1/";
-//"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLnN0YWdpbmcuY3Jvb251cy5jb20vYXBpL3YxL2FkbWluL3NpZ24taW4vbG9naW4iLCJpYXQiOjE2OTM1NzI2MDIsImV4cCI6MTY5MzU3NjIwMiwibmJmIjoxNjkzNTcyNjAyLCJqdGkiOiJlOGc2bHhJVVNIRllqQjJXIiwic3ViIjoiMSIsInBydiI6IjE1NDA5NTUyMDFmYjcxZTVjYjgwZmFhYjRkOGY5ZjQzNjA4YjVlNGMifQ.LqO_WUjWbPdiO40NZfhVzfEGtVCMvBOte7drBmZatSg"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Assert sure base URL for the API set
-if (process.env.REACT_APP_URL || dejan) {
-    localStorage.setItem("api", process.env.REACT_APP_URL || dejan);
+if (process.env.REACT_APP_URL || local) {
+    localStorage.setItem("api", process.env.REACT_APP_URL || local);
 
-    ReactDOM.render(
+    root.render(
         <AuthContextProvider>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-        </AuthContextProvider>,
-        document.getElementById("root")
+        </AuthContextProvider>
     );
 } else {
-    ReactDOM.render(<h1>Required env variable is not set: REACT_APP_URL</h1>, document.getElementById("root"));
+    root.render(<h1>Required env variable is not set: REACT_APP_URL</h1>);
 }
