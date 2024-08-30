cbf3c3f525a7aa65e3c14e85618ab50308420325
marko-lazarevic <57814676+marko-lazarevic@users.noreply.github.com>
2024-08-30 08:30:21 +0200

    B2B static pages SEO

--- src/pages/B2BStaticPages/B2BStaticPagesDetails/panels/Seo.jsx
+++ src/pages/B2BStaticPages/B2BStaticPagesDetails/panels/Seo.jsx
@@ -1,63 +1,21 @@
-import { useContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import List from "../../../../components/shared/ListAdder/List";
-
 import formFields from "../forms/seo.json";
-import AuthContext from "../../../../store/auth-contex";
+import ListPage from "../../../../components/shared/ListPage/ListPage";
 
 const Seo = ({ pageId }) => {
-
-  const init = {
-    id: null,
-    id_static_pages: pageId,
-    slug: null,
-    id_country: null,
-    meta_title: null,
-    meta_keywords: null,
-    meta_description: null,
-    meta_url: null,
-  };
-
-  const [listData, setListData] = useState([]);
-  const authCtx = useContext(AuthContext);
-  const { api } = authCtx;
-  const apiPath = "admin/static-pages-b2b/seo";
-
-  const handleList = () => {
-    api.list(`${apiPath}/${pageId}`)
-      .then((response) => setListData(response?.payload?.items))
-      .catch((error) => console.warn(error));
-  };
-
-  const handleSubmit = (data) => {
-    api.post(apiPath, data)
-      .then((response) => {
-        handleList();
-        toast.success("Uspešno");
-      })
-      .catch((error) => {
-        console.warn(error);
-        toast.warn(error);
-      });
-  };
-
-  const handleDelete = (token, id) => {
-    api.delete(`${apiPath}/${id}`)
-      .then((response) => {
-        handleList();
-        toast.success("Uspešno");
-      })
-      .catch((error) => {
-        console.warn(error);
-        toast.warn(error);
-      });
-  };
-
-  useEffect(() => {
-    handleList();
-  }, []);
-
-  return <List formFields={formFields} init={init} listFields={listData} onSave={handleSubmit} onDelete={handleDelete} />;
+    return (
+        <ListPage
+            listPageId="ProductSeo"
+            apiUrl={`admin/static-pages-b2b/seo/${pageId}`}
+            editUrl={`admin/static-pages-b2b/seo`}
+            deleteUrl={`admin/static-pages-b2b/seo`}
+            title=" "
+            columnFields={formFields}
+            actionNewButton="modal"
+            initialData={{ id_static_pages: pageId }}
+            addFieldLabel="Dodajte novu vrednost"
+            showAddButton={true}
+        />
+    );
 };
 
 export default Seo;
