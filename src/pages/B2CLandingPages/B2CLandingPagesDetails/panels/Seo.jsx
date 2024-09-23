03e6991c589d5a2e268c96dca506d3246e7a45d7
marko-lazarevic <57814676+marko-lazarevic@users.noreply.github.com>
2024-09-23 09:21:23 +0200

    Fixed api paths

--- src/pages/B2CLandingPages/B2CLandingPagesDetails/panels/Seo.jsx
+++ src/pages/B2CLandingPages/B2CLandingPagesDetails/panels/Seo.jsx
@@ -9,7 +9,7 @@ const Seo = ({ pageId }) => {
     const [formFieldsTemp, setFormFieldsTemp] = useState(tblFields);
 
     const handleInformationImage = () => {
-        api.get(`admin/product-items/seo/options/upload`)
+        api.get(`admin/landing-pages-b2c/seo/options/upload`)
             .then((response) => {
                 formatFormFields(response?.payload);
             })
