5a94c80aaf4b3a4b22fad8dbe0a328e0ecff341e
Aleksandar Kastratovic <aleksandar.kastratovic@croonus.com>
2024-12-10 09:39:21 +0100

    86c17anb9 - Fixed copy modal on Product module

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ListPage from "../../components/shared/ListPage/ListPage";
 import tblFields from "./tblFields.json";
 import listCheckbox from "./listCheckbox.json";
@@ -6,13 +6,16 @@ import ModalForm from "../../components/shared/Modal/ModalForm";
 import { useTableCellActions } from "../../hooks/useTableCellActions";
 import { useCellSubmit } from "../../hooks/useCellSubmit";
 import useAPI from "../../api/api";
-import { useMutation } from "react-query";
+import FormWrapper from "../../components/shared/Layout/FormWrapper/FormWrapper";
+import { Typography, Box, CircularProgress } from "@mui/material";
+import Form from "../../components/shared/Form/Form";
+import { toast } from "react-toastify";
 
 const Products = () => {
     const [openModal, setOpenModal] = useState({ show: false, id: null, name: null });
-    const [fields, setFields] = useState(tblFields);
     const [fieldsTmp, setFieldsTmp] = useState();
     const [doesRefetch, setDoesRefetch] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const customActions = {
         contentCopy: {
@@ -61,6 +64,24 @@ const Products = () => {
         }
     }, [doesRefetch]);
 
+    const submitHandler = (data) => {
+        setIsLoading(true);
+        return api
+            .post(`admin/product-items/list/clone`, data)
+            .then((res) => {
+                setIsLoading(false);
+                setDoesRefetch(!doesRefetch);
+                toast.success(`Uspešno`);
+                setOpenModal({ show: false, id: null, name: null });
+                return res?.payload;
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                toast.warning(error?.response?.data?.message ?? error?.response?.data?.payload?.message ?? "Greška");
+                return error;
+            });
+    };
+
     return (
         <>
             <ListPage
@@ -68,7 +89,7 @@ const Products = () => {
                 apiUrl="admin/product-items/list"
                 deleteUrl="admin/product-items/basic-data"
                 title="Proizvodi"
-                columnFields={fields}
+                columnFields={tblFields}
                 customActions={customActions}
                 tableCellActions={{
                     actions: customTableCellActions,
@@ -80,36 +101,40 @@ const Products = () => {
                 doesRefetch={doesRefetch}
                 setDoesRefetch={setDoesRefetch}
             />
-            <ModalForm
-                anchor="right"
-                openModal={openModal}
-                setOpenModal={setOpenModal}
-                formFields={listCheckbox}
-                sx={{ padding: "2rem" }}
-                apiPathFormModal="admin/product-items/list/clone"
-                initialData={{
-                    id_product: openModal.id,
-                    categories: 0,
-                    inventories: 0,
-                    prices: 0,
-                    seo: 0,
-                    crossselles: 0,
-                    upsells: 0,
-                    related: 0,
-                    attributes: 0,
-                    variantsAttributes: 0,
-                    gallery: 0,
-                    docs: 0,
-                }}
-                withoutSetterFunction
-                cancelButton
-                setDoesRefetch={setDoesRefetch}
-                doesRefetch={doesRefetch}
-                label="Dupliraj"
-                styleCheckbox={{ padding: "0 0.563rem 0 0.563rem" }}
-                customTitle={`Da li ste sigurni da želite da duplirate proizvod ${openModal.name}?`}
-                shortText="Dupliranjem se automatski dupliraju sledeći podaci proizvoda: Osnovno, Opis, Deklarijacija. Pored navedenih podataka možete odabrati koje još podatke želite da duplirate za novi proizvod."
-            />
+            <ModalForm anchor="right" openModal={openModal} setOpenModal={setOpenModal} sx={{ padding: "2rem" }} apiPathFormModal="admin/product-items/list/clone">
+                {isLoading ? (
+                    <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                        <CircularProgress size="2rem" sx={{ marginTop: "50vh" }} />
+                    </Box>
+                ) : (
+                    <FormWrapper title={`Da li ste sigurni da želite da duplirate proizvod ${openModal.name}?`}>
+                        <Typography variant="body2" sx={{ marginBottom: "0.8rem" }}>
+                            Dupliranjem se automatski dupliraju sledeći podaci proizvoda: Osnovno, Opis, Deklarijacija. Pored navedenih podataka možete odabrati koje još podatke želite da duplirate za
+                            novi proizvod.
+                        </Typography>
+
+                        <Form
+                            formFields={listCheckbox}
+                            initialData={{
+                                id_product: openModal.id,
+                                categories: 1,
+                                inventories: 1,
+                                prices: 1,
+                                seo: 1,
+                                crossselles: 1,
+                                upsells: 1,
+                                related: 1,
+                                attributes: 1,
+                                variantsAttributes: 1,
+                                gallery: 1,
+                                docs: 1,
+                            }}
+                            onSubmit={submitHandler}
+                            styleCheckbox={{ padding: "0 0.563rem 0 0.563rem" }}
+                        />
+                    </FormWrapper>
+                )}
+            </ModalForm>
         </>
     );
 };
