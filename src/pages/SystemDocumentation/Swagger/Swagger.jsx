2836878a778de0e55d8fe0c2ff599a2ee450b5a7
Aleksandar Kastratovic <aleksandar.kastratovic@croonus.com>
2025-02-20 09:54:45 +0100

    86c11gxu2 - updated Swagger component

--- src/pages/SystemDocumentation/Swagger/Swagger.jsx
+++ src/pages/SystemDocumentation/Swagger/Swagger.jsx
@@ -1,100 +1,102 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import useAPI from "../../../api/api";
 import { useQuery } from "react-query";
+import { Select, MenuItem } from "@mui/material";
+import PageWrapper from "../../../components/shared/Layout/PageWrapper/PageWrapper";
+import "./Swagger.scss";
 
 const Swagger = () => {
-  const [selectedSpec, setSelectedSpec] = useState("");
-  const [yamlFiles, setYamlFiles] = useState([]);
-  const api = useAPI();
+    const [selectedSpec, setSelectedSpec] = useState("");
+    const api = useAPI();
 
-  // Fetch Swagger YAML files
-  const { data: ddlYamlFiles, error, isLoading } = useQuery(
-    "systemDocumentationSwaggerFiles",
-    async () => {
-      return await api.get("admin/system-documentation/swagger/ddl/files")
-        .then((res) => res?.payload ?? [])
-        .catch((error) => {
-          console.error(error);
-          return [];
-        });
-    },
-    { refetchOnWindowFocus: false } // Prevent refetching on window focus
-  );
+    // Fetch Swagger YAML files
+    const {
+        data: ddlYamlFiles,
+        error,
+        isLoading,
+    } = useQuery(
+        "systemDocumentationSwaggerFiles",
+        async () => {
+            return await api
+                .get("admin/system-documentation/swagger/ddl/files")
+                .then((res) => {
+                    let base_api = localStorage.getItem("api") + "admin/system-documentation/swagger/file/".replace(/^\//, "");
+                    if (res?.payload?.[0]?.url) {
+                        setSelectedSpec(base_api + res?.payload?.[0]?.url);
 
-  // Update state when `ddlYamlFiles` changes
-  useEffect(() => {
-    if (ddlYamlFiles) {
-      let base_api = localStorage.getItem("api") + ("admin/system-documentation/swagger/file/").replace(/^\//, "");
-      ddlYamlFiles.map( ( item ) => {
+                        const updatedRes = res.payload.map((item) => {
+                            if (!item.url.startsWith(base_api)) {
+                                item.url = base_api + item.url;
+                            }
+                            return item;
+                        });
 
-        if(!item.url.startsWith(base_api)) {
-          item.url = base_api + item.url;
-        }
-        return item;
-      });
-      
-      setYamlFiles(ddlYamlFiles);
-    }
-  }, [ddlYamlFiles]);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error loading Swagger documentation</p>;
-
-  return (
-    <>
-    <h2>Select API Specification</h2>
-    <select onChange={(e) => setSelectedSpec(e.target.value)} value={selectedSpec}>
-      {yamlFiles.length > 0 && yamlFiles?.map((file, index) => (
-        <option key={index} value={file.url}>
-          {file.name}
-        </option>
-      ))}
-    </select>
-
-    <SwaggerUI
-      url={selectedSpec}
-      supportedSubmitMethods={[
-        "get",
-        "post",
-        "put",
-        "delete",
-        "patch",
-        "options",
-        "head",
-        "trace",
-        "list",
-        "fetch"
-      ]}
-      requestInterceptor={(request) => {
-        request.headers["Authorization"] = `Bearer ${api.user?.access_token}`;
-        return request;
-      }}
-      plugins={[
-        function () {
-          return {
-            statePlugins: {
-              spec: {
-                wrapSelectors: {
-                  operations: (origSelector) => (state) => {
-                    const operations = origSelector(state); // Get the list of operations
-                    console.log("Operations:", operations); // Inspect operations
-                    // You can filter or modify operations here
-                    return operations;
-                  },
-                },
-              },
-            },
-          };
+                        return updatedRes;
+                    } else return [];
+                })
+                .catch((error) => {
+                    console.error(error);
+                    return [];
+                });
         },
-      ]}
-      deepLinking={true}
-      persistAuthorization={true}
-      displayRequestDuration={true}
-    />
-    </>
-  );
+        { refetchOnWindowFocus: false }
+    );
+
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error loading Swagger documentation</p>;
+
+    return (
+        <PageWrapper title={"API Specification"}>
+            <Select
+                onChange={(e) => setSelectedSpec(e.target.value)}
+                value={selectedSpec}
+                sx={{
+                    width: "100%",
+                    "& legend": { display: "none" },
+                    "& fieldset": { top: 0 },
+                    "& .MuiSelect-select": { padding: "0.7rem", fontSize: "0.875rem" },
+                }}
+            >
+                {ddlYamlFiles.length > 0 &&
+                    ddlYamlFiles?.map((file, index) => (
+                        <MenuItem key={index} value={file.url}>
+                            {file.name}
+                        </MenuItem>
+                    ))}
+            </Select>
+
+            <SwaggerUI
+                url={selectedSpec}
+                supportedSubmitMethods={["get", "post", "put", "delete", "patch", "options", "head", "trace", "list", "fetch"]}
+                requestInterceptor={(request) => {
+                    request.headers["Authorization"] = `Bearer ${api.user?.access_token}`;
+                    return request;
+                }}
+                plugins={[
+                    function () {
+                        return {
+                            statePlugins: {
+                                spec: {
+                                    wrapSelectors: {
+                                        operations: (origSelector) => (state) => {
+                                            const operations = origSelector(state); // Get the list of operations
+                                            // You can filter or modify operations here
+                                            return operations;
+                                        },
+                                    },
+                                },
+                            },
+                        };
+                    },
+                ]}
+                deepLinking={true}
+                persistAuthorization={true}
+                displayRequestDuration={true}
+            />
+        </PageWrapper>
+    );
 };
 
 export default Swagger;
